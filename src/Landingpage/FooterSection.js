8764b6a89d36b5595b6ff56a0bdc5d0ed0122838
bishwanath <bishwanath3714312@gmail.com>
2025-03-22 08:12:50 +0530

    footer & landingcss

--- src/Landingpage/FooterSection.js
+++ src/Landingpage/FooterSection.js
@@ -14,37 +14,258 @@ function FooterSection() {
           Largest Luxury Lighting Brand
         </h1>
         <p>
-          An exquisite and extensive collection of luxury lighting awaits you to
-          adorn your beautiful living spaces and transform them beyond your
-          wildest dreams.
-        </p>
-        <p>
-          We are a one-stop solution for all your luxury lighting needs and one
-          visit to our luxury lighting stores anywhere in the country is what
-          you truly need to see what you're missing out on. Over the years, The
-          White Teak Company has established itself as one of the finest
-          manufacturers and retailers of decorative lights, designer ceiling
-          fans, and home decor.
-        </p>
+            An exquisite and extensive collection of luxury lighting awaits you
+            to adorn your beautiful living spaces and transform them beyond your
+            wildest dreams. Embark on an unparalleled journey of opulence and
+            experience luxury like never before with The White Teak Company.
+          </p>
+          <p>
+            We are a one-stop solution for all your luxury lighting needs and
+            one visit to our luxury lighting stores anywhere in the country is
+            what you truly need to see what you're missing out on. Over the
+            years, The White Teak Company has established itself as one of the
+            finest manufacturers and retailers of decorative lights, designer
+            ceiling fans, and home decor, and now we flaunt an effervescent and
+            sublime range of architectural lights too.
+          </p>
 
         <div className={`extra-content ${isExpanded ? "show" : ""}`}>
-          <p>
-            The White Teak Company is India's Largest Decorative Lighting Brand.
-            For years, millions have entrusted us with their dreams, and we've
-            always enabled them to realize their glorious visions of their dream
-            homes.
+        <p>
+            We are a one-stop solution for all your luxury lighting needs and
+            one visit to our luxury lighting stores anywhere in the country is
+            what you truly need to see what you're missing out on. Over the
+            years, The White Teak Company has established itself as one of the
+            finest manufacturers and retailers of decorative lights, designer
+            ceiling fans, and home decor, and now we flaunt an effervescent and
+            sublime range of architectural lights too.
           </p>
           <p>
-            A splendid range of decorative lighting designs awaits you,
-            including chandeliers, ceiling lights, wall lights, pendant lights,
-            table lamps, and much more, each promising to redefine luxury and
-            sophistication in your space.
+            The White Teak Company is India's Largest Decorative Lighting Brand,
+            and one look at our resplendent collection of lighting designs will
+            make you realise we're not exaggerating. For years, millions have
+            entrusted us with their dreams, and we've always enabled them to
+            realise their glorious visions of their dream homes. We know the
+            emotions that go into adorning your fabulous abodes and lighting up
+            every nook and cranny of your living space, and that is why we are
+            with you at every step of designing the lighting layout of your
+            home. The White Teak Company is here to bridge the gap to your dream
+            home and we will stay by your side at every stage of designing the
+            lighting layout of your space. Let The White Teak Company work its
+            magic on your home, visit our decorative lighting store near you
+            now. Or since you've made it till here, you can feast your eyes on
+            the finest luxury lighting collection and home decor online.
+          </p>
+          <h2>
+            Discover the finest collection of decorative lights only at The
+            White Teak Company
+          </h2>
+          <p>
+            The White Teak Company is the best choice you can make for your home
+            and our gorgeous range of decorative lighting designs can seamlessly
+            give your home the makeover of your dreams. The White Teak Company
+            came into existence to counter the unorganised luxury lighting
+            segment of India. The luxury lighting segment in our country is
+            still at a nascent stage and many retailers end up selling
+            decorative lighting with under-par quality at exorbitant prices. The
+            lack of options in the market was one of the reasons why this has
+            continued to happen in the Indian market. The White Teak Company
+            transformed the entire industry with its revolutionary and
+            innovative luxury lighting solutions and our brand has been
+            instrumental in making luxury lighting accessible to the Indian
+            consumers. Indian consumers don't have to settle for less than they
+            deserve, nor do they have to purchase premium or luxury lighting
+            from unreliable retailers.
           </p>
           <p>
-            Step into the world of The White Teak Company and experience a
-            seamless blend of aesthetics and innovation. Our collection is
-            designed to cater to every taste, whether you prefer modern
-            minimalism or vintage elegance.
+            Despite our extraordinary online presence, our ravishing decorative
+            lighting showrooms have been designed to offer an exceptionally
+            elegant and personalised experience to our clientele and any and
+            every luxury design enthusiast that wants to witness luxury like
+            never before. We've always believed in offering the finest luxury
+            lighting showrooms to the people in India and we strive to offer
+            luxury as an experience rather than a commodity. And that will be
+            evident the moment you visit our resplendent lighting stores near
+            you. Witness the sheer resplendence in person and be a part of our
+            enormous White Teak family. Visit a decorative lighting shop near
+            you and once you're done with that, we encourage you to pay a visit
+            to any of our White Teak near you to see the colossal difference
+            between the experiences you have at both places. The White Teak
+            Company is undoubtedly the best choice for you because we want you
+            to have an unforgettable experience that's synonymous with our brand
+            - lush and resplendent. Visit our lighting showroom near you right
+            now. We welcome you to set foot into our world of elegance and see
+            what luxury truly means.
+          </p>
+          <h2>
+            An Exquisite &amp; Elegant Lighting Collection That Will Mesmerise
+            You
+          </h2>
+          <p>
+            The White Teak Company houses an effervescent lighting design that
+            brings out the best of your living space effortlessly. The gorgeous
+            lighting designs add a distinct charm to your decor and they will
+            transform your living space forever. We offer a stunning range of
+            lighting designs that can change the visuospatial perception of your
+            space along with accentuating the aesthetic value and the
+            functionality of your decor. If you've been planning to give your
+            home a perfect makeover then you've found the right address.
+          </p>
+          <p>
+            You are highly unlikely to find an exhaustive decorative collection
+            quite like The White Teak Company's across the country. We bring you
+            an extensive range of
+            <span style={{display:"inline-block"}}>decorative lighting</span>
+            fixtures, including
+            <span style={{display:"inline-block"}}>chandeliers</span>,<span style={{display:"inline-block"}}>pendant lights</span>,
+            <span style={{display:"inline-block"}}>table lamps</span>,<span style={{display:"inline-block"}}>floor lamps</span>,
+            <span style={{display:"inline-block"}}>ceiling lights</span>,<span style={{display:"inline-block"}}>wall lights</span>,
+            <span style={{display:"inline-block"}}>outdoor lights</span>, and more. Visiting any one of our
+            luxury lighting stores near you, you'll get hands-on experience with
+            our products, with their designs, with the cutthroat precision that
+            went into their making and also the exceptional quality that we
+            offer. All of our lighting fixtures are brought to life with extreme
+            care and love so that they can add an unparalleled aura and charm to
+            your beautiful abode. Not many luxury lighting and home decor stores
+            in India can offer a build and quality quite like ours and that is
+            precisely why we want you to witness The White Teak Company's
+            expertise first-hand.
+          </p>
+          <p>
+            The White Teak Company is simply the best choice for you, even if
+            you're looking for a modern yet practical range of
+            <span>architectural lighting</span>
+            designs to set up the foundation for your lighting layout. Our
+            architectural lighting designs will add a contemporary flair to your
+            living space, and they'll change the way anybody looks at your decor
+            forever. We are the one-stop solution for all your lighting needs,
+            and you'll find the finest range of lighting designs for your home,
+            workplace or any kind of place of business that needs an effortless
+            touch of elegance. We design every lighting fixture with absolute
+            finesse and exceptional craftsmanship so that they can add
+            unparalleled charm to your living space. Visit our decorative
+            lighting and home decor store near you now to see exactly what we're
+            talking about. We assure you that you'll find luxury lighting
+            designs that you've never ever set your eyes on before and your
+            understanding of luxury and elegance is bound to change. See you at
+            our luxury lighting showroom soon.
+          </p>
+          <h2>
+            Explore an extensive array of decorative lighting design styles with
+            The White Teak Company
+          </h2>
+          <p>
+            Our extensive and exclusive lighting collection flaunts the best
+            blend of the plushest design languages and the most luxe elements.
+            No matter what you like or what you prefer, you'll find the perfect
+            designs for your decor in our vivid and effervescent lighting
+            collection. We bring the finest modern, contemporary, transitional,
+            traditional, classical, rustic, vintage, industrial lighting designs
+            and much more that will add a distinct flair to your living space.
+          </p>
+          <p>
+            At The White Teak Company, we love playing around with design styles
+            to create lighting designs that add just the right poise and panache
+            to your living space. While we do embrace the latest design trends,
+            we give enough love to the modern design styles of yesteryears. LED
+            lighting has transformed the global industry and we make sure that
+            our designs use the technology to the best of our ability to
+            redefine grandeur unlike any other. Make sure that you check out our
+            <span>Dimmable LED Collection</span>
+            and see how our LED decorative lights can transform the ambience of
+            your home seamlessly. And it's just our contemporary designs that
+            make use of LED lighting, we blend the technology with our modern
+            fixtures too that are reminiscent of the design styles from the
+            other eras. For example, you'll find the best LED lighting bulbs at
+            The White Teak Company that resemble the incandescent and halogen
+            lights that added an unmatched warmth to your living space. Now that
+            is something you'll find difficult to get anywhere else. Our vintage
+            LED lighting designs can add just the magic and class in your space
+            that you've always sought.
+          </p>
+          <h2>
+            Embark on an unparalleled journey of opulence with The White Teak
+            Company
+          </h2>
+          <p>
+            The White Teak Company came into existence back in 2016, and we have
+            transformed the Indian luxury lighting space with our exquisite and
+            innovative lighting designs. There's something distinct about our
+            designs, and they're just the ones you need to give your home an
+            effervescent touch of elegance. Visit any of our luxury lighting
+            stores across the country and feast your eyes on the finest luxury
+            decorative lights you've ever seen. Be a part of The White Teak
+            family, and embark on this remarkable journey of opulence.
+          </p>
+          <p>
+            The White Teak Company is a name you can trust in the luxury
+            lighting segment of India. We have become a household name in the
+            luxury lighting industry because our enormous clientele has let us
+            turn their dreams into reality. We will always be here to give a
+            fine touch to your decor, and we'll help you lend an unmatched
+            stroke of opulence to your home. This is your chance to experience
+            luxury like never before. Visit any of our decorative lighting
+            stores near you now.
+          </p>
+          <h2>
+            Why is The White Teak's decorative lighting the best choice for you
+          </h2>
+          <p>
+            Our journey started back in 2016 because we realised what a consumer
+            truly wants and what the lighting stores in India couldn't offer.
+            Despite an enormous demand for luxury lighting and home decor in
+            India, our founders realised that luxury lighting is one of the most
+            ignored aspects of interior design. And that is the key reason why
+            The White Teak Company came into existence, to provide world-class
+            lighting at world-class prices. One look at the ravishing lighting
+            designs from The White Teak Company is enough to give you an idea of
+            what we are capable of. Our lighting designs can beautifully adorn
+            your living space and transform it beyond your wildest dreams.
+          </p>
+          <p>
+            Be it the exquisite designs, the effervescent collections, or the
+            unbreakable trust, The White Teak Company will always be the best
+            choice you can make for your home. Our lighting designs won't just
+            be an investment that you make or an asset that you own, but they'll
+            seamlessly blend into your decor to become a part of your home
+            beautifully. You'll never find luxury lighting fixtures like ours
+            anywhere in the market.
+          </p>
+          <h2>
+            The idea behind The White Teak Company's enormous decorative
+            lighting collection
+          </h2>
+          <p>
+            Our company has picked its name from a very exotic and exquisite
+            type of teak of the same name. White teak originates from India, and
+            the wood and the artefacts made from it are considered a symbol of
+            luxury and opulence in most parts of the world. And that is exactly
+            what we wanted to replicate in our brand. No matter if you're trying
+            to find elegant chandeliers, pendant lights, floor lamps, ceiling
+            lights or table lamps in India, The White Teak collection will
+            definitely outshine other luxury lighting and home decor stores in
+            the city.
+          </p>
+          <p>
+            Find any of our decorative lighting stores near you in
+            {" "}<span style={{display:"inline-block"}}>Bangalore</span>,<span style={{display:"inline-block"}}>Delhi</span>,<span style={{display:"inline-block"}}>Hyderabad</span>,
+            <span style={{display:"inline-block"}}>Noida</span>,<span style={{display:"inline-block"}}>Chennai</span>,<span style={{display:"inline-block"}}>Mumbai</span>,
+            <span style={{display:"inline-block"}}>Pune</span>
+            and Ludhiana to lay your eyes on the best luxury lighting fixtures
+            ever. The White Teak Company is always working on making your
+            purchase experience as convenient as possible. And you'll find that
+            pretty evident when you visit our stores. We offer an exceptional
+            online shopping experience, and you'll find everything you're
+            looking for here.
+          </p>
+          <p>
+            The White Teak Company is India's Largest Decorative Lighting Brand,
+            and we're here to redefine luxury in the industry and help you stay
+            ahead of the curve. India's luxury lighting market fails to keep up
+            with the trends of the global markets, but that won't be an issue
+            when you shop with The White Teak Company. At The White Teak
+            Company, our limit is our imagination, which is exactly what made us
+            one of the most favourable luxury lighting brands in India. Hope to
+            see you at one of our luxury lighting showrooms very soon.
           </p>
         </div>
 
